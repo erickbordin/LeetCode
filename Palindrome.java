c3285e1032740abbbf078ea71981f28a289de282
Érick Bordin <ericksouzabordin3@gmail.com>
2026-02-10 23:41:11 -0300

    criei um projeto twosum

---
+++ Palindrome.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Palindrome {
+
+    public static boolean isPalindrome(int x) {
+
+        String numeroVef = String.valueOf(x);
+
+        if (numeroVef.length() == 1) {
+            return true;
+        }
+
+        int j = numeroVef.length() - 1;
+
+        for (int i = 0; i < numeroVef.length(); i++) {
+
+            if (numeroVef.charAt(i) != numeroVef.charAt(j)) {
+                return false;
+            }
+
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        boolean ret = isPalindrome(entrada.nextInt());
+
+        System.out.println(ret);
+
+        entrada.close();
+    }
+
+}
---
+++ TwoSum.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+class Solution {
+
+    public static int[] twoSum(int[] nums, int target) {
+
+        System.out.println("ola");
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return (new int[] { i, j });
+                }
+            }
+
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+        int[] resultado = twoSum(new int[] { 2, 7, 11, 15 }, 18);
+        System.out.println(Arrays.toString(resultado));
+    }
+
+}
