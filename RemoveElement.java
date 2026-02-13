394e578b44bc39de2e73b21e51c5bb8e58ca864f
Érick Bordin <ericksouzabordin3@gmail.com>
2026-02-13 00:45:15 -0300

    more one solution

---
+++ RemoveElement.java
@@ -0,0 +1,26 @@
+
+class Solution {
+    public int removeElement(int[] nums, int val) {
+
+        int contador = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[contador] = nums[i];
+                contador++;
+
+            }
+        }
+        return contador;
+    }
+
+    public static void main(String[] args) {
+
+        Solution s1 = new Solution();
+
+        int[] numeros = { 3, 2, 2, 3 };
+
+        s1.removeElement(numeros, 2);
+
+    }
+
+}
